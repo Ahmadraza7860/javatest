abaf372e8a39efae372fec8bad2fa65a1e08df35
Ahmadraza7860 <130864259+Ahmadraza7860@users.noreply.github.com>
2023-06-24 18:03:26 +0530

    script.js

--- script.js
+++ script.js
@@ -1,41 +1,19 @@
+const apiUrl =
+  'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false';
 
-  let arr = [
-    { id: 1, name: "john", age: "18", profession: "developer" },
-    { id: 2, name: "jack", age: "20", profession: "developer" },
-    { id: 3, name: "karen", age: "19", profession: "admin" },
-  ];
-
-  function PrintDeveloperbyMap() {
-    const developers = arr.filter(employee => employee.profession === "developer");
-    developers.map(developer => console.log(developer));
-  }
-
-  function PrintDeveloperbyForEach() {
-    arr.forEach(employee => {
-      if (employee.profession === "developer") {
-        console.log(employee);
-      }
-    });
-  }
-
-  function addData() {
-    const newEmployee = { id: 4, name: "susan", age: "20", profession: "intern" };
-    arr.push(newEmployee);
-    console.log(newEmployee);
-  }
-
-  function removeAdmin() {
-    const filteredEmployees = arr.filter(employee => employee.profession !== "admin");
-    console.log(filteredEmployees);
-  }
-
-  function concatenateArray() {
-    const newArray = [
-      { id: 4, name: "Alice", age: "22", profession: "designer" },
-      { id: 5, name: "Bob", age: "25", profession: "manager" },
-      { id: 6, name: "Eve", age: "24", profession: "developer" }
-    ];
-    const combinedArray = arr.concat(newArray);
-    console.log(combinedArray);
-  }
-
+// Fetching data using .then
+fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => {
+    // Iterate over the data array and populate the table
+    for (let i = 0; i < data.length; i++) {
+      const crypto = data[i];
+      document.getElementById(`name-${i}`).textContent = crypto.name;
+      document.getElementById(`id-${i}`).textContent = crypto.id;
+      document.getElementById(`image-${i}`).innerHTML = `<img src="${crypto.image}" alt="${crypto.name}" width="25">`;
+      document.getElementById(`symbol-${i}`).textContent = crypto.symbol;
+      document.getElementById(`price-${i}`).textContent = crypto.current_price;
+      document.getElementById(`volume-${i}`).textContent = crypto.total_volume;
+    }
+  })
+  .catch(error => console.error(error));
